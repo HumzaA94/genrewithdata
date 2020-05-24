2e02edc0b302506ccc50e4b83c24343a3e6dd0a2
hahmed0111 <humza1910@gmail.com>
2020-05-24 02:16:05 -0400

    moving over tab_functions

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import plotly.graph_objs as go
 import pandas as pd
 import sqlalchemy
 import psycopg2
+
+from tabs import tab_functions as tf
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -16,25 +18,8 @@ server = app.server
 app.title='GenreWithData'
 ########### Set up the layout
 
-
-dict_columns = ['TACTICS','PASS', 'CARRY', 'UNDER_PRESSURE', 'BALL_RECEIPT', 'COUNTERPRESS',
-                'INTERCEPTION', 'DRIBBLE', 'GOALKEEPER', 'SHOT', 'OUT', 'DUEL', 'BALL_RECOVERY',
-                'CLEARANCE', 'OFF_CAMERA', 'FOUL_WON', 'FOUL_COMMITTED', 'SUBSTITUTION',
-                'INJURY_STOPPAGE', 'MISCONTROL', '50_50', 'BAD_BEHAVIOUR', 'BLOCK', 'PLAYER_OFF',
-                'HOME_COUNTRY', 'AWAY_COUNTRY', 'STADIUM_COUNTRY', 'REFEREE_COUNTRY']
-button_dict={
-    'button_1':'statsbomb.competition_information',
-    'button_2':'statsbomb.match_information',
-    'button_3':'nba_reference.player_overall_seasons',
-    'button_4':'nba_reference.player_stats_by_game'
-    }
-
-def generate_banner(title):
-    return html.Label(children=title)
-
-
 app.layout = html.Div([
-    generate_banner('Improve the banner style, want to keep the get info here?'),
+    tf.generate_banner('Improve the banner style, want to keep the get info here?'),
     dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
         dcc.Tab(label='Tab One', value='tab-1-example'),
         dcc.Tab(label='Query Tab', value='tab-2-example'),
---
+++ tabs/tab_functions.py
@@ -0,0 +1,101 @@
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+import dash
+
+import plotly.graph_objs as go
+import pandas as pd
+import sqlalchemy
+import psycopg2
+
+dict_columns = ['TACTICS','PASS', 'CARRY', 'UNDER_PRESSURE', 'BALL_RECEIPT', 'COUNTERPRESS',
+                'INTERCEPTION', 'DRIBBLE', 'GOALKEEPER', 'SHOT', 'OUT', 'DUEL', 'BALL_RECOVERY',
+                'CLEARANCE', 'OFF_CAMERA', 'FOUL_WON', 'FOUL_COMMITTED', 'SUBSTITUTION',
+                'INJURY_STOPPAGE', 'MISCONTROL', '50_50', 'BAD_BEHAVIOUR', 'BLOCK', 'PLAYER_OFF',
+                'HOME_COUNTRY', 'AWAY_COUNTRY', 'STADIUM_COUNTRY', 'REFEREE_COUNTRY']
+button_dict={
+    'button_1':'statsbomb.competition_information',
+    'button_2':'statsbomb.match_information',
+    'button_3':'nba_reference.player_overall_seasons',
+    'button_4':'nba_reference.player_stats_by_game'
+    }
+
+# try:
+#     engine = sqlalchemy.create_engine(
+#         'postgresql://root:a12r324$lrjfSSp-3L14#^167fa@awsdatabase.cka4r7aphadv.us-east-2.rds.amazonaws.com/sportsdb')
+#
+# except:
+#     print("Unable to connect to db")
+#
+# def read_sql(string, engine=engine):
+#     try:
+#         df = pd.read_sql(string, con=engine)
+#         return df
+#
+#     except sqlalchemy.exc.ProgrammingError as e:
+#         msg=str(e)
+#         return msg
+
+def create_table(div_id,df,page_no):
+    return (dash_table.DataTable(
+        id=div_id,
+        columns=[{"name": i, "id": i, "deletable": True, "selectable": True} for i in df.columns],
+        data=df.to_dict('records'),
+        editable=True,
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        column_selectable="single",
+        row_selectable="multi",
+        row_deletable=True,
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current= 0,
+        page_size= page_no,
+        export_format='csv',
+        style_table={'overflowX': 'scroll'},
+        style_cell = {
+                'text_align': 'center'}))
+
+def generate_banner(title):
+    return html.Label(children=title)
+
+def generate_button(val,btn_id,outline=True):
+    return dbc.Button(val, id=btn_id, outline=outline,className="buttons",n_clicks=0)
+
+def generate_range_slider(div_id,min,max,marks):
+    return dcc.RangeSlider(
+        id=div_id,
+        min=min,
+        max=max,
+        step=1,
+        marks=marks,
+        value=[min, max])
+
+def single_value_dropdown(div_id,label_name,list1,text):
+    return html.Div(
+    children = [      #if more than 1 container will have the exact feature, a classname might be more convenient
+    html.Label(children=label_name),
+    html.Br(),
+    dcc.Dropdown(
+            id= div_id, #container_id used to design the container of the dropdown in css,
+            options=[{'label': l, 'value': l} for l in list1],
+            placeholder=text,
+            style={'display':'inline-block','width':'400px','height':'30px'})])
+
+def multi_value_dropdown(div_id,label_name,list1):
+    return html.Div(
+    children = [
+    html.Label(children=label_name),
+    html.Br(),
+    dcc.Dropdown(
+    id= div_id,
+    options=[{'label': l, 'value': l} for l in list1],
+    value=list1,
+    style={'display':'inline-block','width':'500px','height':'150px'},
+    multi=True
+    ),],
+    )
