0c0530389071e2fac6a1b15ce88aa13786fbf325
hahmed0111 <humza1910@gmail.com>
2020-05-24 02:32:32 -0400

    testing postgres with query tab

--- main.py
+++ main.py
@@ -35,7 +35,40 @@ def render_content(tab):
         return dropdown_tab.tab_1_layout
     elif tab == 'tab-2-example':
         return query_tab.tab_2_layout
+# #callbacks for query_tab
+@app.callback(Output('intermediate-value','children'),
+              [Input('button_1', 'n_clicks'),
+               Input('button_2', 'n_clicks'),
+               Input('button_3','n_clicks'),
+               Input('button_4','n_clicks'),
+               Input('button_21','n_clicks')],
+              [State('sql_script','value')])
+def update_query(btn1,btn2,btn3,btn4,btn21,val):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    try:
+        button_val=re.split("\.",changed_id)[0]
+        if button_val=='button_21':
+            return val
+        else:
+            val1='SELECT * FROM {} LIMIT 10;'.format(tf.button_dict[button_val])
+            return  val1
+    except KeyError as e:
+        return None
 
+@app.callback(Output('output-container', 'children'),
+              [Input('intermediate-value', 'children')])
+def displayClick(val):
+    if val is None:
+        return html.Div(style={'display': 'none'})
+    else:
+        df=tf.read_sql(val)
+        if isinstance(df,str):
+            return html.Div(children=[df])
+        else:
+            for c in df.columns:
+                if c in tf.dict_columns:
+                    df[c]=df[c].astype(str)
+            return html.Div(tf.create_table('query_table',df,10))
 ########### Run the app
 if __name__ == '__main__':
     server.run(debug=True, port=8080)
